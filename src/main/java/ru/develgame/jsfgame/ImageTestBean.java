1c1946886ce8257b915cb3c5cc0bf5c776ac3836
izemskov <Ilya.Zemskov@telekom.com>
2022-08-18 14:19:35 +0300

    Moving

--- src/main/java/ru/develgame/jsfgame/ImageTestBean.java
+++ src/main/java/ru/develgame/jsfgame/ImageTestBean.java
@@ -1,7 +1,6 @@
-package ru.develgame.javaeejsf;
+package ru.develgame.jsfgame;
 
 import javax.enterprise.context.SessionScoped;
-import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import java.io.Serializable;
 
@@ -10,11 +9,26 @@ import java.io.Serializable;
 public class ImageTestBean implements Serializable {
     private int currentFrame = 1;
 
+    private int imageTop = 0;
+
     public String loadImage() {
         String image = "/images/walk_down" + currentFrame + ".png";
         currentFrame++;
         if (currentFrame > 8)
             currentFrame = 1;
         return image;
+    }
+
+    public String getNewTopPosition() {
+        imageTop = imageTop + 2;
+        return Integer.toString(imageTop);
+    }
+
+    public int getImageTop() {
+        return imageTop;
+    }
+
+    public void setImageTop(int imageTop) {
+        this.imageTop = imageTop;
     }
 }
