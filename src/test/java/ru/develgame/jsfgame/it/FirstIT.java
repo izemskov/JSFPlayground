8a32c8712ed8fb7726c6b05d19d03340a0453a89
izemskov <pascal.ilya@gmail.com>
2023-06-12 23:15:35 +0300

    Tmp 2

--- src/test/java/ru/develgame/jsfgame/it/FirstIT.java
+++ src/test/java/ru/develgame/jsfgame/it/FirstIT.java
@@ -2,9 +2,10 @@ package ru.develgame.jsfgame.it;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import ru.develgame.jsfgame.MyBean;
@@ -18,15 +19,16 @@ public class FirstIT {
     @Inject
     private MyBean myBean;
 
-    @Deployment(testable = false)
-    public static WebArchive createDeployment() {
-        return ShrinkWrap.create(WebArchive.class)
-                .addClass(MyBean.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    @Deployment
+    public static JavaArchive createTestArchive() {
+        return ShrinkWrap.create(JavaArchive.class, "test.jar")
+                .addClasses(MyBean.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"));
     }
 
     @Test
     public void testUserBean() {
         assertNotNull(myBean);
+        assertNotNull(myBean.getGuid());
     }
 }
